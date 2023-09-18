fb7f92b41df27e5280507ac6a8c88445b568fb81
KimDowi <rlaehdnl@gmail.com>
2023-09-18 22:40:32 +0900

    2023-09-18

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,22 +1,41 @@
 <template>
   <div class="name">
-    {{ greet }}
+    {{ name }}
   </div>
+  <br>
+  <button 
+    class="btn btn-primary"
+    v-on:click="updateName"
+  >
+    Click
+  </button>
 </template>
 
 <script>
+import { ref, reactive } from 'vue';
+
 export default{
   setup(){
-    const name = 'Kossie Coder1';
+    // const name = ref('Kossie Coder1');
+    const name = reactive({
+      id: 1
+    });
 
-    const greeting = (name) => {
-      return 'Hello, ' + name;
+    // const greeting = (name) => {
+    //   return 'Hello, ' + name;
+    // };
+
+    // const greet = greeting(name);
+
+    const updateName = () =>{
+      // name.value = 'Kossie Coder'; ref는 value.id
+      name.id += 1; // reactive는 value 없이 사용
+      console.log(name);
     };
 
-    const greet = greeting(name);
-
     return {
-      greet
+      name,
+      updateName
     };
   }
 }
