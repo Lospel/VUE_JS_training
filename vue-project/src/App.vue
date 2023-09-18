b0e489b97a740b2ba6529d94c90f92b69be4f36c
KimDowi <rlaehdnl@gmail.com>
2023-09-18 22:21:46 +0900

    2023-09-18

---
+++ vue-project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="name">
+    {{ greet }}
+  </div>
+</template>
+
+<script>
+export default{
+  setup(){
+    const name = 'Kossie Coder1';
+
+    const greeting = (name) => {
+      return 'Hello, ' + name;
+    };
+
+    const greet = greeting(name);
+
+    return {
+      greet
+    };
+  }
+}
+</script>
+
+<style>
+  .name {
+    color: red;
+  }
+</style>
