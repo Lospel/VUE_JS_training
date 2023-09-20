749997fe2bfc4ec662abfbb6970ce48a27d6eb59
KimDowi <rlaehdnl@gmail.com>
2023-09-20 22:36:18 +0900

    2023-09-20

---
+++ vue_example/src/examples/Conditionals and Loops.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {ref} from 'vue'
+
+const show = ref(true)
+const list = ref([1, 2, 3])
+</script>
+
+<template>
+  <button @click="show = !show">Toggle List</button>
+  <button @click="list.push(list.length + 1)">Push Number</button>
+  <button @click="list.pop()">Pop Number</button>
+  <button @click="list.reverse()">Reverse List</button>
+
+  <ul v-if="show && list.length">
+    <li v-for="item of list">{{ item }}</li>
+  </ul>
+  <p v-else-if="list.length">배열은 남아있지만 보이지 않습니다.</p>
+  <p v-else>배열이 비워졌습니다.</p>
+</template>
